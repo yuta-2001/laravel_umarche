7ff60c930466110eec8cbd0c0222b430ad36e3c1
yuta-2001 <yu0210ta.sug@gmail.com>
2021-09-04 16:54:38 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
         ヘッダー２
     </x-slot>
     コンポーネントテスト２
+    <x-test-class-base classBaseMessage="メッセージです"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultmessage="初期値から変更しています"/>
 </x-tests.app>
