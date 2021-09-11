b4c0446d9f2a6edf881caac27e45899324a51618
yuta-2001 <yu0210ta.sug@gmail.com>
2021-09-11 17:02:27 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -41,7 +41,7 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all, $q_get, $q_first, $c_test);
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
 
 
         return view('admin.owners.index',
@@ -109,6 +109,9 @@ class OwnersController extends Controller
     public function edit($id)
     {
         //
+        $owner = Owner::findOrFail($id);
+        // dd($owner);
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -121,6 +124,16 @@ class OwnersController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email= $request->email;
+        $owner->password = Hash::make($request->password);
+        // dd($owner);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー情報を更新しました。');
     }
 
     /**
