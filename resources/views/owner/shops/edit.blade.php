54ecb06b9c8ea8eea33fd75fe2214c2b7e2dc8d4
yuta-2001 <yu0210ta.sug@gmail.com>
2021-09-18 00:10:22 +0900

    shop edit,update

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -13,16 +13,45 @@
                     <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id]) }}" enctype="multipart/form-data">
                         @csrf
                         <div class="-m-2">
-                          <div class="p-2 w-1/2 mx-auto">
-                            <div class="relative">
-                              <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
-                              <input type="file" id="image" name="image" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="name" class="leading-7 text-sm text-gray-600">店名 ※必須</label>
+                                    <input required type="text" id="name" name="name" value="{{ $shop->name }}"class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
                             </div>
-                          </div>
-                          <div class="p-2 w-full mt-4 flex justify-around">
-                            <button type="button" onclick="location.href='{{ route('owner.shops.index') }}'" accept="image/png,image/jpeg,image/jpg" class=" bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                            <button type="submit" class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
-                          </div>
+
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
+                                    <textarea required id="information" name="information" rows="10" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>
+                                </div>
+                            </div>
+
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <x-shop-thumbnail :filename="$shop->filename" />
+                                </div>
+                            </div>
+
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
+                                    <input type="file" id="image" name="image" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div><input type="radio" name="is_selling" class="mr-2" value="1" @if($shop->is_selling === 1){ checked } @endif>販売中</div>
+                                    <div><input type="radio" name="is_selling" class="mr-2" value="0" @if($shop->is_selling === 0){ checked } @endif>停止中</div>
+                                </div>
+                            </div>
+
+                            <div class="p-2 w-full mt-4 flex justify-around">
+                                <button type="button" onclick="location.href='{{ route('owner.shops.index') }}'" accept="image/png,image/jpeg,image/jpg" class=" bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+                                <button type="submit" class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
+                            </div>
+
                         </div>
                     </form>
                 </div>
