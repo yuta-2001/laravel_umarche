742efdd22aac5cb9bcc7e136efffd52ea5ede10a
yuta-2001 <yu0210ta.sug@gmail.com>
2021-09-28 11:45:20 +0900

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -38,8 +39,13 @@ class ItemController extends Controller
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+
+        //同期的に送信
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
+
+        //非同期に送信
+        SendThanksMail::dispatch();
 
         $products = Product::availableItems()
         ->selectCategory($request->category ?? '0')
